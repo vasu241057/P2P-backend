402556c7aa97dc94b01b82ca79b69e832e0b4be6
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-09-27 12:47:32 +0530

    port changed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
